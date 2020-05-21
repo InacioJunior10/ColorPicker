f53ee95356e5287b0ca65ad98ff4d73ec9cb24d0
InacioJunior10 <inaciojunior10@hotmail.com>
2020-05-21 20:28:02 -0300

    Primeira refatoração

--- js/script.js
+++ js/script.js
@@ -1,99 +1,49 @@
 window.addEventListener('load', start);
 
-var inputR = null;
-var inputG = null;
-var inputB = null;
-
-var slideR = null;
-var slideG = null;
-var slideB = null;
+var inputR = 0;
+var inputG = 0;
+var inputB = 0;
 
 function start() {
-  slideR = document.querySelector('#slideR');
-  slideG = document.querySelector('#slideG');
-  slideB = document.querySelector('#slideB');
+  document.querySelector('#slideR').addEventListener('input', rangeValue);
+  document.querySelector('#slideG').addEventListener('input', rangeValue);
+  document.querySelector('#slideB').addEventListener('input', rangeValue);
 
-  slideR.addEventListener('input', setRed);
-  slideG.addEventListener('input', setGreen);
-  slideB.addEventListener('input', setBlue);
-  setDefaultColor();
+  setColor(0, 0, 0);
 }
 
-function setDefaultColor() {
-  inputR = 0;
-  inputG = 0;
-  inputB = 0;
-
-  var slideR = document.querySelector('#slideR');
-  var slideG = document.querySelector('#slideG');
-  var slideB = document.querySelector('#slideB');
-  slideR.value = inputR;
-  slideG.value = inputG;
-  slideB.value = inputB;
-
-  var textR = document.querySelector('#textValueR');
-  var textG = document.querySelector('#textValueG');
-  var textB = document.querySelector('#textValueB');
-  textR.value = slideR.value;
-  textG.value = slideG.value;
-  textB.value = slideB.value;
-
-  setColor();
+function rangeValue(event) {
+  switch (event.target.id) {
+    case 'slideR':
+      inputR = event.target.value;
+      document.querySelector('#textValueR').value = inputR;
+      setColor(inputR, inputG, inputB);
+      break;
+    case 'slideG':
+      inputG = event.target.value;
+      document.querySelector('#textValueG').value = inputG;
+      setColor(inputR, inputG, inputB);
+      break;
+    case 'slideB':
+      inputB = event.target.value;
+      document.querySelector('#textValueB').value = inputB;
+      setColor(inputR, inputG, inputB);
+      break;
+  }
 }
 
-function setRed(event) {
-  var value = event.target.value;
-  inputR = value;
-  var textR = document.querySelector('#textValueR');
-  textR.value = value;
-
-  setColor();
-}
-
-function setGreen(event) {
-  var value = event.target.value;
-  inputG = value;
-  var textG = document.querySelector('#textValueG');
-  textG.value = value;
-
-  setColor();
-}
-
-function setBlue(event) {
-  var value = event.target.value;
-  inputB = value;
-  var textB = document.querySelector('#textValueB');
-  textB.value = value;
-
-  setColor();
-}
-
-function setColor() {
+function setColor(r, g, b) {
   var divColor = document.querySelector('#divColor');
-  var backgroundColor = 'rgb(' + inputR + ', ' + inputG + ', ' + inputB + ')';
+  var backgroundColor = 'rgb(' + r + ', ' + g + ', ' + b + ')';
   divColor.style.backgroundColor = backgroundColor;
   toHexString();
 }
 
+// Converter RGB em Hexadecimal
 function toHexString() {
-  // var r = toHex(inputR);
-  // var g = toHex(inputG);
-  // var b = toHex(inputB);
-
-  // var textHex = document.querySelector('#hex');
-  // textHex.value = '#' + r + g + b;
-
   var textHex = document.querySelector('#hex');
   var backgroundColor = 'rgb(' + inputR + ', ' + inputG + ', ' + inputB + ')';
   textHex.value = rgb2hex(backgroundColor);
-}
-
-function toHex(n) {
-  var hex = n.toString(16);
-  while (hex.length < 2) {
-    hex = '0' + hex;
-  }
-  return hex;
 }
 
 function rgb2hex(rgb) {
